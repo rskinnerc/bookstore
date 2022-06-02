970e52178aa92baa331e0c5d3f2ef8051c111f9d
Ronald <rjskinnerc@unal.edu.co>
2022-06-02 10:33:48 -0500

    Update the book component

--- src/components/Book.js
+++ src/components/Book.js
@@ -3,10 +3,25 @@ import PropTypes from 'prop-types';
 const Book = ({ title, author }) => {
   return (
     <div>
-      Book Component
-      {title}
-      ,
-      {author}
+      <div>
+        <span>Action</span>
+        <h2>{title}</h2>
+        <h3>{author}</h3>
+        <div>
+          <button type="button">Comments</button>
+          <button type="button">Remove</button>
+          <button type="button">Edit</button>
+        </div>
+      </div>
+      <div>
+        <span>64%</span>
+        <span>Completed</span>
+      </div>
+      <div>
+        <h3>CURRENT CHAPTER</h3>
+        <span>Chapter 17</span>
+        <button type="button">UPDATE PROGRESS</button>
+      </div>
     </div>
   );
 };
