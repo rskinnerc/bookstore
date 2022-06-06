2697f03acf181b972501d710205f79a7c4b127b1
Ronald <rjskinnerc@unal.edu.co>
2022-06-06 16:04:46 -0500

    Update store config to add reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,6 +1,11 @@
 import { combineReducers, createStore } from 'redux';
+import books from './books/books';
+import categories from './categories/categories';
 
-const reducers = combineReducers({});
+const reducers = combineReducers({
+  books,
+  categories,
+});
 const bookstoreStore = createStore(reducers);
 
 export default bookstoreStore;
