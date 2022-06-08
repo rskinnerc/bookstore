988842292df40d5f758aaf1c9004022d4c85651c
Ronald <rjskinnerc@unal.edu.co>
2022-06-08 16:39:27 -0500

    Style the form

--- src/components/Book.js
+++ src/components/Book.js
@@ -34,7 +34,7 @@ const Book = ({
 
       </div>
       <div className="chapter-info">
-        <h3 className="curent-chapter">CURRENT CHAPTER</h3>
+        <h3 className="current-chapter">CURRENT CHAPTER</h3>
         <span className="chapter">Chapter 17</span>
         <button type="button" className="update-progress">UPDATE PROGRESS</button>
       </div>
